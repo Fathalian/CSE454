18dc9bc713b649a7c84f65f00803e35ede50aa31
Siavash Baratchi <barats@uw.edu>
2012-01-25 22:09:54 -0800

    script

---
+++ script.js
@@ -0,0 +1,39 @@
+function checkForm(form)
+{
+if(form.username.value==""){
+	alert("Error: Username cannot be blank!");
+	form.username.focus();return false;
+}
+re=/^\w+$/;
+if(!re.test(form.username.value)){
+	alert("Error: Username must contain only letters, numbers and underscores!");
+	form.username.focus();return false;
+}
+if(form.pwd1.value!="")
+{if(form.pwd1.value.length<6){
+alert("Error: Password must contain at least six characters!");
+form.pwd1.focus();return false;
+}
+if(form.pwd1.value==form.username.value){
+alert("Error: Password must be different from Username!");
+form.pwd1.focus();return false;
+}
+re=/[0-9]/;if(!re.test(form.pwd1.value)){
+alert("Error: password must contain at least one number (0-9)!");
+form.pwd1.focus();return false;
+}
+re=/[a-z]/;if(!re.test(form.pwd1.value)){
+alert("Error: password must contain at least one lowercase letter (a-z)!");
+form.pwd1.focus();return false;
+}
+re=/[A-Z]/;if(!re.test(form.pwd1.value)){
+alert("Error: password must contain at least one uppercase letter (A-Z)!");
+form.pwd1.focus();return false;
+}
+}
+else{alert("Error: Password cannot be blank!");
+form.pwd1.focus();
+return false;
+}
+
+return true;}
